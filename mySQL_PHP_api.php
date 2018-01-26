d947839e5e6c4cefc25f942d61ec17abe2373bae
PriyadharshanSaba <priyadharshan.97@gmail.com>
2018-01-26 14:26:27 +0530

    JSON output

--- mySQL_PHP_api.php
+++ mySQL_PHP_api.php
@@ -6,33 +6,58 @@
     //API for mySQL database connection for mobile applications. Returns data in JSON format
     //Runtime: PHP 7.1.13
 
-    $mysqli = new mysqli("localhost", "user", "password", "database");
+    
+    header("Content-Type:application/json");
 
-    $result = $mysqli->query("  ");
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        $conn_status="Failed";
+    //Checks the URL for attributes sent via GET method
+    if(!empty($_GET['userid']))
+    {
+        $user=$_GET['userid'];
+        $res = fetchFromDB($user);
+
+        //response in JSON
+        if(empty($res))
+        {
+            jsonResponse(200,"Not Found",NULL);
+        }
+        else
+        {
+            jsonResponse(200,"Found",$res);
+        }
     }
-    $conn_status="Success";
-    ?>
-
-<html>
-<head>
-<title>Testing</title>
-</head>
-
-<body>
-
-
-<h1 style="text-align:center">Establishing Connection</h1><br><br>
-<?php
-    while ($row = $result->fetch_assoc()) {
-        $res = $row["USR_ID"];
-        echo "<h2 style='text-align:center'> $res </h2>";
+    else
+    {
+        jsonResponse(400,"Invalid Request",NULL);
     }
-    ?>
 
+    //the function to fetch details from Database
+    function fetchFromDB($user)
+    {
+        //mySQL connector
+        $mysqli = new mysqli("localhost", "user", "password", "database");
+        $result = $mysqli->query("  "); //Query field
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            $conn_status="Failed";
+        }
+        $conn_status="Success";
+        while ($row = $result->fetch_assoc()) {
+            $res = $row["USR_PSW"];
+        }
+        return $res;
+    }
 
-</body>
-</html>
+    function jsonResponse($status,$status_message,$data)
+    {
+        //JSON format
+        $response['status']=$status;
+        $response['status_message']=$status_message;
+        $response['fetched']=$data;
+
+        $json_response = json_encode($response);
+        echo $json_response;
+    }
+
+?>
+
 
